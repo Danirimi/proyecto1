31dd91a17f47776c6814130c20a99a8b37c03f03
ASUS <ASUS@LAPTOP-1TFCB4KD>
2025-07-03 22:18:10 -0600

    Calduladora

---
+++ src/proyecto1/Cliente.java
@@ -0,0 +1,114 @@
+//Libreria
+package proyecto1;
+//Variables
+public class Cliente {
+private int Opcion ;
+public int Numero;
+private String Letra;
+//Constructor    
+    public Cliente(int Opcion,int Numero,String Letra) {
+        this.Opcion = Opcion;
+        this.Numero = Numero;
+        this.Letra = Letra;
+    }
+    
+//Get-Set Opcion
+    public int getOpcion() {
+        return Opcion;
+    }
+
+    public void setOpcion(int Opcion) {
+        this.Opcion = Opcion;
+    }
+    
+//Get-Set Numero
+    public int getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(int Numero) {
+        this.Numero = Numero;
+    }
+//Get-Set Letra
+
+    public String getLetra() {
+        return Letra;
+    }
+
+    public void setLetra(String Letra) {
+        this.Letra = Letra;
+    }
+    
+    
+//Metodo Tickete-Prioridad
+   public void Prioridad(){
+       Numero=Numero++;
+       switch (Opcion){
+        case 1:{
+        Letra="A";
+        String Ticket1 = Integer.toString(Numero);
+        String Ticket2 = Ticket1+Letra;
+        System.out.println("Su numero de Tikete es ="+Ticket2);
+        break;
+    } 
+        case 2:{
+        Numero=Numero+1;
+        Letra="B";
+        String Ticket1 = Integer.toString(Numero);
+         String Ticket2 = Ticket1+Letra;
+        System.out.println("Su numero de Tikete es ="+Ticket2);
+        break;
+    } 
+           case 4:{
+        Numero=Numero+1;
+        Letra="C";
+        String Ticket1 = Integer.toString(Numero);
+         String Ticket2 = Ticket1+Letra;
+        System.out.println("Su numero de Tikete es ="+Ticket2);
+        break;        
+    } 
+              case 5:{
+        Numero=Numero+1;
+        Letra="D";
+        String Ticket1 = Integer.toString(Numero);
+         String Ticket2 = Ticket1+Letra;
+        System.out.println("Su numero de Tikete es ="+Ticket2);
+        break;
+    } 
+            case 6:{
+        Numero=Numero+1;
+        Letra="E";
+        String Ticket1 = Integer.toString(Numero);
+         String Ticket2 = Ticket1+Letra;
+        System.out.println("Su numero de Tikete es ="+Ticket2);
+        break;
+    }
+        case 7:{
+        Numero=Numero+1;
+        Letra="F";
+        String Ticket1 = Integer.toString(Numero);
+         String Ticket2 = Ticket1+Letra;
+        System.out.println("Su numero de Tikete es ="+Ticket2);
+        break;
+    }     
+        case 8:{
+        Numero=Numero+1;
+        Letra="G";
+        String Ticket1 = Integer.toString(Numero);
+         String Ticket2 = Ticket1+Letra;
+        System.out.println("Su numero de Tikete es ="+Ticket2);
+        break;
+        } 
+    }
+}
+    
+    
+   }
+
+
+         
+ 
+
+
+
+
--- src/proyecto1/Proyecto1.java
+++ src/proyecto1/Proyecto1.java
@@ -1,20 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package proyecto1;
 
-/**
- *
- * @author danir
- */
+package proyecto1;
+import java.util.Scanner;
+
 public class Proyecto1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
-}
+    public static void main(String[] args) {
+        String Letra ="Vacio";
+        int NumeroTicket = 0;
+        while (NumeroTicket<25){
+        Scanner Sc = new Scanner (System.in);
+        System.out.println("1= persona adulta");
+        System.out.println("2= mujer embarazada");
+        System.out.println("3= persona con alguna discapacidad");
+        System.out.println("4= dos o más asuntos a tratar");
+        System.out.println("5=atención de la Plataforma");
+        System.out.println("6= mujer");
+        System.out.println("7= hombre");
+        int Opcion = Sc.nextInt();
+        Cliente Prioridad = new Cliente (Opcion,NumeroTicket,Letra);
+        Prioridad.Prioridad();
+    }    
+    }
+    }
+
